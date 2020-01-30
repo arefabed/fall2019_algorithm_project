3416997af8a4c1b5dd3dc7d9949e9c0fd5432395
arefabed <arefabed24@gmail.com>
2020-01-30 15:43:33 +0330

    plt_naccidents_vs_dates.py v1

---
+++ plt_naccidents_vs_dates.py
@@ -0,0 +1,23 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import datetime as dt
+
+df = [pd.read_csv('data/accidents_2005_to_2007.csv', usecols=['Date']),
+      pd.read_csv('data/accidents_2009_to_2011.csv', usecols=['Date']),
+      pd.read_csv('data/accidents_2012_to_2014.csv', usecols=['Date'])]
+
+c = {2005: 0, 2006: 0, 2007: 0, 2009: 0, 2010: 0, 2011: 0, 2012: 0, 2013: 0, 2014: 0}
+for data in df:
+    temp = data["Date"]
+    for i in temp:
+        date_time_obj = dt.datetime.strptime(i, '%d/%m/%Y')
+        c[date_time_obj.year] += 1
+
+
+plt.bar(list(c.keys()), list(c.values()))
+
+plt.xlabel('date')
+plt.ylabel('#accidents ')
+plt.title('annual accidents ', fontsize=20)
+plt.legend()
+plt.show()
