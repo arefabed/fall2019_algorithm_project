91ffa330f4e1ec81dc2ba55ddbf21f96a8ad268d
arefabed <arefabed24@gmail.com>
2020-02-03 09:16:25 +0330

    update fork

---
+++ update_fork.sh
@@ -0,0 +1,6 @@
+git remote add upstream https://github.com/Amir-Mehrpanah/fall2019_algorithm_project
+git fetch upstream
+git rebase upstream/master
+git pull
+echo "press any key to continue..."
+read
