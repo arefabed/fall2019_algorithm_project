636b4ec23caf9d60b8ea02f305442bb26e61b74e
arefabed <arefabed24@gmail.com>
2020-02-03 13:16:40 +0330

    change5

--- py_files/kind_of_vehicle.py
+++ py_files/kind_of_vehicle.py
@@ -12,7 +12,7 @@ def kind_of_vehicle():
     
     print('computing...')
     c = {2000: 0, 2001: 0, 2002: 0, 2003: 0, 2004: 0, 2005: 0, 2006: 0, 2007: 0,
-         2008: 0, 2009: 0, 2010: 0, 2011: 0, 2012: 0, 2013: 0, 2014: 0, 2015: 0, 2016: 0}
+         2008: 0, 2009: 0, 2010: 0, 2011: 0, 2012: 0, 2013: 0, 2014: 0, 2015: 0,              2016: 0}
     vehicle = {'PedalCycles': 0, 'Motorcycles': 0,
                 'CarsTaxis': 0, 'BusesCoaches': 0,
                 'LightGoodsVehicles': 0, 'V2AxleRigidHGV': 0}
