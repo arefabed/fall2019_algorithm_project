6b245650386bcf0e8220423bc94fa66201852d7b
arefabed <arefabed24@gmail.com>
2020-02-10 16:17:41 +0330

    ncasualties_per_year

--- py_files/plot_ncasualties_per_year.py
+++ py_files/plot_ncasualties_per_year.py
@@ -7,13 +7,13 @@ def ncasualties_per_year():
     df = [pd.read_csv('data/accidents_2005_to_2007.csv', usecols=['Year', 'Number_of_Casualties']),
           pd.read_csv('data/accidents_2009_to_2011.csv', usecols=['Year', 'Number_of_Casualties']),
           pd.read_csv('data/accidents_2012_to_2014.csv', usecols=['Year', 'Number_of_Casualties'])]
-    udf = pd.concat(df)
+
     print('computing...')
 
     c = {2005: 0, 2006: 0, 2007: 0, 2008: 0, 2009: 0,
          2010: 0, 2011: 0, 2012: 0, 2013: 0, 2014: 0}
-
-    for row_ind in tqdm(udf.index):
-        row = udf.iloc[row_ind, :]
-        c[row['Year']] += row['Number_of_Casualties']
+    for item in df:
+        for row_ind in tqdm(item.index):
+           row = item.iloc[row_ind, :]
+           c[row['Year']] += row['Number_of_Casualties']
     return c
