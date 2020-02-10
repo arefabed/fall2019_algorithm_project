ca1f6f44cbf9a67040e26953be707346ac7f88df
arefabed <arefabed24@gmail.com>
2020-02-10 15:25:19 +0330

    ncasualties_per_year

--- py_files/plot_naccident_per_road_type.py
+++ py_files/plot_naccident_per_road_type.py
@@ -6,7 +6,7 @@ def naccident_per_road_type():
 
     df = pd.read_csv('data/ukTrafficAADF.csv',
                      usecols=['AADFYear', 'LinkLength_km',
-                              'PedalCycles', 'Motorcycles',
+                              'Road_Type', 'Motorcycles',
                               'CarsTaxis', 'BusesCoaches',
                               'LightGoodsVehicles', 'V2AxleRigidHGV'])
 
@@ -14,3 +14,4 @@ def naccident_per_road_type():
 
     c = {2005: 0, 2006: 0, 2007: 0, 2008: 0, 2009: 0,
          2010: 0, 2011: 0, 2012: 0, 2013: 0, 2014: 0}
+
---
+++ py_files/plot_ncasualties_per_year.py
@@ -0,0 +1,19 @@
+def ncasualties_per_year():
+    import pandas as pd
+    from tqdm import tqdm
+
+    print('reading files...')
+
+    df = [pd.read_csv('data/accidents_2005_to_2007.csv', usecols=['Year', 'Number_of_Casualties']),
+          pd.read_csv('data/accidents_2009_to_2011.csv', usecols=['Year', 'Number_of_Casualties']),
+          pd.read_csv('data/accidents_2012_to_2014.csv', usecols=['Year', 'Number_of_Casualties'])]
+    udf = pd.concat(df)
+    print('computing...')
+
+    c = {2005: 0, 2006: 0, 2007: 0, 2008: 0, 2009: 0,
+         2010: 0, 2011: 0, 2012: 0, 2013: 0, 2014: 0}
+
+    for row_ind in tqdm(udf.index):
+        row = udf.iloc[row_ind, :]
+        c[row['Year']] += row['Number_of_Casualties']
+    return c
